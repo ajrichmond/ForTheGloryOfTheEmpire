1d64036b06d69ebe9a14cebbb953e95d6070f85c
Adam Richmond <44481796+ajrichmond@users.noreply.github.com>
2022-04-29 16:24:09 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,29 +1,38 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(fpp3)
+library(tsibble)
+library(seasonal)
+library(forecast)
+library(caret)
+library(lubridate)
+library(fpp3)
+library(parallel)
+library(doParallel)
 
-library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+CREDITDATA <- read.csv('credit.csv')
 
-    # Application title
-    titlePanel("For the Glory of the Empire"),
+CREDITDATA$Month <- 492:1
+CREDITDATA$Month <- yearmonth(CREDITDATA$Month)
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            
-        ),
+CREDIT_TS <- tsibble(CREDITDATA, index = Month)
+CREDIT_TS %>% 
+  stretch_tsibble()
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            
-        )
-    )
-))
+autoplot(CREDIT_TS)
+gg_season(CREDIT_TS)
+
+#lambda <- CREDIT_TS %>%
+#features(Month, features = guerrero) %>%
+#pull(lambda_guerrero)
+
+#CREDIT_TS %>%
+#autoplot(box_cox(Month, lambda))
+
+CREDIT_TS %>% 
+  filter(year(Month) >= '1970 Feb') -> CREDIT_TS
+
+CREDIT_TS %>% 
+  filter(year(Month) <= '2004 Jan') -> TRAINCREDIT
+
+
+
