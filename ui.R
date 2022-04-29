fc72a8865793846fffe9fcc2df268d54a1d2c969
Adam Richmond <44481796+ajrichmond@users.noreply.github.com>
2022-04-29 16:13:27 -0400

    CREDIT_TS
    Look at basic formatting of data and fix/clean if necessary 
    Convert to tsibble 
    Autoplot the tsibble 
    Train Data(?)

--- ui.R
+++ ui.R
@@ -7,23 +7,38 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+library(fpp3)
+library(tsibble)
+library(seasonal)
+library(forecast)
+library(caret)
+library(lubridate)
+library(fpp3)
+library(parallel)
+library(doParallel)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("For the Glory of the Empire"),
+CREDITDATA <- read.csv('credit.csv')
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            
-        ),
+CREDITDATA$Month <- 492:1
+CREDITDATA$Month <- yearmonth(CREDITDATA$Month)
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            
-        )
-    )
-))
+CREDIT_TS <- tsibble(CREDITDATA, index = Month)
+CREDIT_TS %>% 
+  stretch_tsibble()
+
+autoplot(CREDIT_TS)
+gg_season(CREDIT_TS)
+
+#lambda <- CREDIT_TS %>%
+  #features(Month, features = guerrero) %>%
+  #pull(lambda_guerrero)
+
+#CREDIT_TS %>%
+  #autoplot(box_cox(Month, lambda))
+
+CREDIT_TS %>% 
+  filter(year(Month) >= '1970 Feb') -> CREDIT_TS
+
+CREDIT_TS %>% 
+  filter(year(Month) <= '2004 Jan') -> TRAINCREDIT
